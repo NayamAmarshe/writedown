52135cf76da06a2e135c7fa189c276449127cf93
presi300 <canaryisle1@hotmail.com>
2023-02-09 22:28:37 +0200

    Signup page centered

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -6,17 +6,21 @@ const Signup: NextPage = () => {
     <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-gradient-to-r from-purple-700 to-blue-500">
       <div className='bg-white bg-opacity-40 py-20 px-12 mx-5 rounded-xl'>
         <h1 className="flex text-center justify-center font-normal text-3xl pb-12">Sign Up</h1>
-        <div className="pl-2">
-          <input type="email" id="input-label" className="my-2 py-3 px-4 flex w-56 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-white dark:border-gray-700 dark:text-black" placeholder="Enter E-Mail" />
+        <div className="">
+          <input type="email" id="input-label" className="my-2 py-3 px-4 flex w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-white dark:border-gray-700 dark:text-black" placeholder="Enter E-Mail" />
 
-          <input type="password" id="input-label" className="my-2 py-3 px-4 flex w-56 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-white dark:border-gray-700 dark:text-black" placeholder="Enter Password" />
+          <input type="password" id="input-label" className="my-2 py-3 px-4 flex w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-white dark:border-gray-700 dark:text-black" placeholder="Enter Password" />
 
-          <button type="button" className=" w-20 py-3 px-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-black text-white shadow-sm align-middle hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm">
+          <div className="w-full pt-1">
+          <button type="button" className="w-20 mr-3 py-3 px-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-black text-white shadow-sm align-middle hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm">
             Sign Up
           </button>
-          <button type="button" className="mx-2 w-50 py-3 px-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm">
+          <button type="button" className="w-35 py-3 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm">
             Have an account?
-          </button>
+          </button>  
+          </div>
+
+          
         </div>
 
 
