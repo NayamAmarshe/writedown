08e827af92c54890016205f1c9755ead4dcb5aab
presi300 <canaryisle1@hotmail.com>
2023-02-10 21:53:06 +0200

    Added finishing touches to SigUp page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -7,26 +7,30 @@ import { FcGoogle } from "react-icons/fc"
 const Signup: NextPage = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
+      {/*Main div*/}
+      
       <div className="flex flex-col gap-10 rounded-xl border bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:shadow-slate-700/[.7] md:p-5">
+        {/*Border div*/}
         <div>
           <h3 className="text-center text-xl font-semibold">
             Welcome to WriteDown
           </h3>
         </div>
         <div className="flex flex-col gap-2">
+        {/*Buttons div*/}
           <p className="text-center text-sm font-semibold mb-2">
             Sign Up with one of the following:
           </p>
-          <Button variant="outline" >
+          <Button variant="white" >
             <FcGoogle /> Sign Up with Google
           </Button>
-          <Button variant="primary" >
+          <Button variant="black" >
             <AiFillGithub /> Sign Up with GitHub
           </Button>
         </div>
         <span className="flex justify-center gap-1">
           <p className="">Already have an account?</p>
-          <a href="/signup" className="font-bold hover:underline">
+          <a href="login" className="font-bold hover:underline">
             Sign In
           </a>
         </span>
