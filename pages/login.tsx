5f7daab63faa1fe20747a25d10942cbaf765e2c7
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-02-10 21:30:53 +0530

    Updated Button and added react-icons

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import Button from '../components/Button'
+import React from "react";
+import Button from "../components/Button";
+import { AiFillGithub, AiFillGoogleCircle } from "react-icons/ai";
 
 const login = () => {
-    return (
-        <div className="flex min-h-screen flex-col items-center justify-center py-2">
-            <div className='bg-white shadow-sm border py-24 px-10 mx-5 rounded-xl'>
-                <h4 className='text-center mb-4 text-sm'>Login with one of the following:</h4>
-                <Button text="Sign in with GitHub" type='custom' customStyle='bg-black text-white hover:bg-gray-800 mb-6' iconSrc='/github.svg' iconStyle='w-6 invert' />
-                <Button text="Sign in with Google" type='secondary' iconSrc='/google.svg' iconStyle='w-6' />
-                <span className='flex justify-center text-sm mt-10'><h4 className='mr-2'>No account yet?</h4><a href="/signup" className='font-bold hover:underline'>Sign Up</a></span>
-            </div>
-        </div >
-    )
-}
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <div className="bg-white shadow-sm border py-24 px-10 mx-5 rounded-xl">
+        <h4 className="text-center mb-4 text-sm">
+          Login with one of the following:
+        </h4>
+        <Button type="primary">
+          <AiFillGithub /> Sign in with GitHub
+        </Button>
+        <Button type="secondary">
+          <AiFillGoogleCircle />
+        </Button>
+        <span className="flex justify-center text-sm mt-10">
+          <h4 className="mr-2">No account yet?</h4>
+          <a href="/signup" className="font-bold hover:underline">
+            Sign Up
+          </a>
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default login
+export default login;
