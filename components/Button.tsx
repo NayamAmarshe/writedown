1abf0ed464b4f33e8f8ecbbafbbbcb7cb3c237ad
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-02-10 22:03:26 +0530

    Fixed preline and login

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 
 interface IProps {
-  type: "primary" | "secondary" | "custom";
+  type: "primary" | "outline" | "ghost";
   children?: React.ReactNode;
 }
 
-const Button = ({ children }: IProps) => {
+const Button = ({ children, type }: IProps) => {
+  const getButtonClass = () => {
+    switch (type) {
+      case "primary":
+        return "inline-flex items-center justify-center gap-2 rounded-md border border-transparent bg-blue-500 py-3 px-4 text-sm font-semibold text-white transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800";
+      case "outline":
+        return "py-[.688rem] px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-blue-500 hover:text-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:border-gray-700 dark:hover:border-blue-500";
+      case "ghost":
+        return "py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm";
+      default:
+        return "inline-flex items-center justify-center gap-2 rounded-md border border-transparent bg-blue-500 py-3 px-4 text-sm font-semibold text-white transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800";
+    }
+  };
+
   return (
-    <button
-      type="button"
-      className={`py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-gray-500 text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition-all text-sm dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-offset-gray-800`}>
+    <button type="button" className={getButtonClass()}>
       {children}
     </button>
   );
