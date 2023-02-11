d31ad8831d38b31bb6ba4a599bbaa5234f4bbcbd
ImRayy <souradeep20roy01@gmail.com>
2023-02-11 13:42:37 +0530

    Update Input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,14 +1,5 @@
-// import React from 'react'
-
-// const Input = () => {
-//   return (
-//     <input type="text" className="py-3 px-5 block w-full border rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" placeholder="Input text"></input>
-//   )
-// }
-
-// export default Input
-
-import React, { ButtonHTMLAttributes, InputHTMLAttributes } from "react";
+import { InputHTMLAttributes } from "react";
+import React from "react";
 
 const Input = ({ ...rest }: InputHTMLAttributes<HTMLInputElement>) => {
   return (
