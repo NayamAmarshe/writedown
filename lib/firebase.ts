a9221ffbc52214e6d21e5f219d5bac0efc26c8b1
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-02-14 10:33:28 +0530

    Added try catch on DbPersistence

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -17,18 +17,22 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const db = getFirestore(firebaseApp);
 
-enableIndexedDbPersistence(db).catch((err) => {
-  if (err.code == "failed-precondition") {
-    // Multiple tabs open, persistence can only be enabled
-    // in one tab at a a time.
-    // ...
-  } else if (err.code == "unimplemented") {
-    // The current browser does not support all of the
-    // features required to enable persistence
-    // ...
-  } else {
-    console.log("ENABLED");
-  }
-});
+try {
+  enableIndexedDbPersistence(db).catch((err) => {
+    if (err.code == "failed-precondition") {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code == "unimplemented") {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    } else {
+      console.log("ENABLED");
+    }
+  });
+} catch (error) {
+  console.log("ERROR", error);
+}
 
 export { db, firebaseApp };
