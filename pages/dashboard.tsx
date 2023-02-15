3b1f0855804e6fda3dce4c67bb68410cf83fb3c9
ImRayy <souradeep20roy01@gmail.com>
2023-02-15 12:38:14 +0530

    Removed chat input

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -35,9 +35,7 @@ const Dashboard = () => {
           />
         )}
       </div>
-      <div>
-        <ChatList user={authUser} />
-      </div>
+      <div>{/* <ChatList user={authUser} /> */}</div>
     </div>
   );
 };
