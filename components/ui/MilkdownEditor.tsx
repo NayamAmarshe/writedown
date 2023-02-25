844a4f02031997312003bc9ebea0a72dce5fb838
Suvojit Ghosh <ghoshsuvojit2012@gmail.com>
2023-02-25 14:49:55 +0530

    added milkdown plugin for clipboard support

--- components/ui/MilkdownEditor.tsx
+++ components/ui/MilkdownEditor.tsx
@@ -8,7 +8,9 @@ import {
 import { listener, listenerCtx } from "@milkdown/plugin-listener";
 import { IChannelData } from "@/types/utils/firebaseOperations";
 import { commonmark } from "@milkdown/preset-commonmark";
+import { clipboard } from "@milkdown/plugin-clipboard";
 import { Milkdown, useEditor } from "@milkdown/react";
+import { history } from "@milkdown/plugin-history";
 import { replaceAll } from "@milkdown/utils";
 import { nord } from "@milkdown/theme-nord";
 import React, { useEffect } from "react";
@@ -49,6 +51,8 @@ const MilkdownEditor = ({
       .config(nord)
       .use(listener)
       .use(commonmark)
+      .use(history)
+      .use(clipboard)
   );
 
   useEffect(() => {
