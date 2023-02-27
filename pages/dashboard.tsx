a378dc54b52340ec9cf1585e2c91d2f53d2e96ca
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-02-27 18:52:46 +0530

    Added ChatScreen component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,17 @@
+import {
+  channelConverter,
+  messagesConverter,
+} from "@/utils/firestoreDataConverter";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
+import ChatScreen from "@/components/dashboard/ChatScreen";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { createUser } from "@/utils/firebaseOperations";
 import ChatList from "@/components/dashboard/ChatList";
 import Sidebar from "@/components/dashboard/Sidebar";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import { db } from "@/lib/firebase";
 import { auth } from "./_app";
 
 const Dashboard = () => {
@@ -11,10 +19,8 @@ const Dashboard = () => {
   const router = useRouter();
 
   // AUTH STATE HOOK
-  const [authUser] = useAuthState(auth, {
+  const [user] = useAuthState(auth, {
     onUserChanged: async (user) => {
-      console.log("🚀 => file: dashboard.tsx:16 => user:", user);
-
       if (!user) {
         router.push("/login");
         return;
@@ -23,21 +29,27 @@ const Dashboard = () => {
     },
   });
 
-  const [showSidebar, setShowSidebar] = useState(false);
+  const [channels] = useCollectionData(
+    user &&
+      query(
+        collection(db, "users", user.uid, "channels"),
+        orderBy("updatedAt", "desc")
+      ).withConverter(channelConverter)
+  );
+
+  const [messages] = useCollectionData(
+    user &&
+      query(
+        collection(db, "messages"),
+        orderBy("createdAt", "desc")
+      ).withConverter(messagesConverter)
+  );
 
   return (
     <div className="flex h-screen w-screen flex-row">
-      <div className="lg:w-96">
-        {authUser && (
-          <Sidebar
-            id="sidebar"
-            showSidebar={showSidebar}
-            setShowSidebar={setShowSidebar}
-            user={authUser}
-          />
-        )}
-      </div>
-      <ChatList user={authUser} />
+      {user && (
+        <ChatScreen channels={channels} messages={messages} user={user} />
+      )}
     </div>
   );
 };
--- utils/firestoreDataConverter.ts
+++ utils/firestoreDataConverter.ts
@@ -1,6 +1,10 @@
+import { IChannelData, IMessageData } from "@/types/utils/firebaseOperations";
 import { QueryDocumentSnapshot } from "firebase/firestore";
 
 export const converter = <T>() => ({
   toFirestore: (data: T) => data,
   fromFirestore: (snap: QueryDocumentSnapshot) => snap.data() as T,
 });
+
+export const channelConverter = converter<IChannelData>();
+export const messagesConverter = converter<IMessageData>();
