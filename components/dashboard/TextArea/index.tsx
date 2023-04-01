67c6f95dcab1a72a3a86d00a3b3fce2f3262e204
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-04-01 22:36:30 +0530

    Change sidebar default state & Added Input

--- components/dashboard/TextArea/index.tsx
+++ components/dashboard/TextArea/index.tsx
@@ -8,7 +8,7 @@ const TextArea = ({ shiftRight }: TextAreaProps) => {
   return (
     <div className="flex w-full items-start justify-center overflow-y-auto">
       <div className="mt-52 h-full w-full max-w-3xl rounded-xl bg-white p-5 transition-transform duration-300">
-        Text Area
+        <input type="text" />
       </div>
     </div>
   );
