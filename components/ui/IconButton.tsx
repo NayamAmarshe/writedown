989b55f8197afd63c260c57599bf49f245c9a8dd
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-04-06 22:26:11 +0530

    Hid sidebar button

--- components/ui/IconButton.tsx
+++ components/ui/IconButton.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 type IconButtonProps = {
   children: React.ReactNode;
+  extraClasses?: HTMLButtonElement["className"];
 };
 
 const IconButton = ({
   children,
+  extraClasses,
   ...rest
 }: IconButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
-      className="absolute top-1/2 -right-5 z-10 rounded-full bg-white p-3 shadow-lg shadow-slate-400/40"
+      className={
+        "absolute top-1/2 -right-5 z-10 rounded-full bg-white p-3 shadow-lg shadow-slate-400/40 " +
+        extraClasses
+      }
       {...rest}
     >
       {children}
