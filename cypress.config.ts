202c749e0a6aed2d807e0dfa3ec89b1cf03cc887
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-04-07 18:36:38 +0530

    Added test

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,4 +7,10 @@ export default defineConfig({
       bundler: "webpack",
     },
   },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
