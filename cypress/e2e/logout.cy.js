b70b2f3ecce44127a2c8bc94a929d4248248f502
TGS963 <ghoshsuvojit2012@gmail.com>
2023-04-09 19:46:10 +0530

    finalized tests

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -7,12 +7,11 @@
 describe("logout checks", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/dashboard");
-    cy.get("div").should("have.class", "hydrated");
   });
 
   it("Logout check", () => {
-    cy.get("#logout").click();
+    cy.findByTestId("logout").click();
     cy.location("pathname").should("eq", "/login");
-    cy.get('button:contains("Sign in with Google")');
+    cy.findByTestId("google-login");
   });
 });
