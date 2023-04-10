cf8e1614fe1b70c1dc351667111074bb6912c8e4
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-04-10 17:43:05 +0530

    Added comments

--- components/ui/MilkdownEditor.tsx
+++ components/ui/MilkdownEditor.tsx
@@ -14,6 +14,7 @@ import { commonmark } from "@milkdown/preset-commonmark";
 import { Milkdown, useEditor } from "@milkdown/react";
 import javascript from "refractor/lang/javascript";
 import typescript from "refractor/lang/typescript";
+import { useAtom, useAtomValue } from "jotai";
 import { math } from "@milkdown/plugin-math";
 import { replaceAll } from "@milkdown/utils";
 import python from "refractor/lang/python";
@@ -31,7 +32,6 @@ import cpp from "refractor/lang/cpp";
 import toast from "react-hot-toast";
 import "katex/dist/katex.min.css";
 import c from "refractor/lang/c";
-import { useAtom } from "jotai";
 
 interface editorProps {
   input: string;
@@ -41,76 +41,86 @@ interface editorProps {
 }
 
 const MilkdownEditor = ({ setInput, input, className, notes }: editorProps) => {
-  const [selectedNoteId, setSelectedNoteId] = useAtom(selectedNoteIdAtom);
-  const editor = useEditor((root) =>
-    Editor.make()
-      .config((ctx) => {
-        ctx.set(rootCtx, root);
-        ctx.set(defaultValueCtx, input);
-        ctx.update(editorViewOptionsCtx, (prev) => ({
-          ...prev,
-          attributes: {
-            class: className ? className : "",
-          },
-        }));
+  const selectedNoteId = useAtomValue(selectedNoteIdAtom);
 
-        ctx.set(prismConfig.key, {
-          configureRefractor: (refractor) => {
-            refractor.register(css);
-            refractor.register(javascript);
-            refractor.register(typescript);
-            refractor.register(jsx);
-            refractor.register(tsx);
-            refractor.register(c);
-            refractor.register(cpp);
-            refractor.register(java);
-            refractor.register(python);
-            refractor.register(rust);
-            refractor.register(yaml);
-            refractor.register(shell);
-            refractor.register(json);
-          },
-        });
+  const editor = useEditor(
+    (root) =>
+      Editor.make()
+        .config((ctx) => {
+          ctx.set(rootCtx, root);
 
-        ctx.set(historyKeymap.key, {
-          // Remap to one shortcut.
-          Undo: "Mod-z",
-          // Remap to multiple shortcuts.
-          Redo: ["Mod-y", "Shift-Mod-z"],
-        });
+          // Default value
+          ctx.set(defaultValueCtx, input);
 
-        ctx.get(listenerCtx).markdownUpdated((_, markdown, prevMarkdown) => {
-          if (markdown.includes("data:image") && prevMarkdown) {
-            editor.get()?.action(replaceAll(prevMarkdown));
-            toast.error(
-              "Please paste a link to an image, pasting images from clipboard is not supported yet"
-            );
-          }
+          // Editor View config
+          ctx.update(editorViewOptionsCtx, (prev) => ({
+            ...prev,
+            attributes: {
+              class: className ? className : "",
+            },
+          }));
 
-          if (markdown !== prevMarkdown) {
-            setInput(markdown);
-          }
-        });
-      })
-      .use(listener)
-      .use(commonmark)
-      .use(gfm)
-      .use(prism)
-      .use(history)
-      .use(math)
+          // Prism plugin config
+          ctx.set(prismConfig.key, {
+            // Register languages
+            configureRefractor: (refractor) => {
+              refractor.register(css);
+              refractor.register(javascript);
+              refractor.register(typescript);
+              refractor.register(jsx);
+              refractor.register(tsx);
+              refractor.register(c);
+              refractor.register(cpp);
+              refractor.register(java);
+              refractor.register(python);
+              refractor.register(rust);
+              refractor.register(yaml);
+              refractor.register(shell);
+              refractor.register(json);
+            },
+          });
+
+          ctx.set(historyKeymap.key, {
+            // Remap to one shortcut.
+            Undo: "Mod-z",
+            // Remap to multiple shortcuts.
+            Redo: ["Mod-y", "Shift-Mod-z"],
+          });
+
+          ctx.get(listenerCtx).markdownUpdated((_, markdown, prevMarkdown) => {
+            // If the user pastes an image, we don't want to save it to the database
+            if (markdown.includes("data:image") && prevMarkdown) {
+              editor.get()?.action(replaceAll(prevMarkdown));
+              toast.error(
+                "Please paste a link to an image, pasting images from clipboard is not supported yet"
+              );
+            }
+
+            // If the current markdown is different from the previous markdown, update the input
+            if (markdown !== prevMarkdown) {
+              setInput(markdown);
+            }
+          });
+        })
+        .use(listener) // Listener for listening to events
+        .use(commonmark) // Commonmark is the default preset
+        .use(gfm) // GFM for GitHub Flavored Markdown
+        .use(prism) // Prism for code highlighting
+        .use(history) // History for undo/redo
+        .use(math) // Math for math typesetting
   );
 
   useEffect(() => {
+    // If there are no notes or no selected note, clear the editor
     if (!notes || !selectedNoteId) {
       editor.get()?.action(replaceAll(""));
 
       return;
     }
 
+    // Replace the editor content with the current note content
     const currentNote = notes.find((note) => note.id === selectedNoteId);
-
     if (!currentNote) return;
-
     editor.get()?.action(replaceAll(currentNote.content));
   }, [selectedNoteId, notes]);
 
