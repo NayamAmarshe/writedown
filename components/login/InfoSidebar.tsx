c2a3c990fa18af58070efe40b6605eeab6a3ea3e
ImRayy <souradeep20roy01@gmail.com>
2023-04-16 23:34:10 +0530

    Updated login

--- components/login/InfoSidebar.tsx
+++ components/login/InfoSidebar.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const InfoSidebar = () => {
-  return <div className="flex h-full w-1/2 flex-col gap-2 bg-slate-200"></div>;
+  return (
+    <div className="hidden h-full w-1/2 flex-col gap-2 bg-slate-200 md:flex">
+      <div className="flex h-3/5 items-end pb-10 pl-10">
+        <p className="text-xl font-medium">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet
+          doloribus, itaque quibusdam ut necessitatibus voluptate officia.
+        </p>
+      </div>
+      <div className="h-full ">
+        <div
+          className="h-full w-full translate-x-10 rounded-xl"
+          style={{
+            background:
+              "url(https://placehold.co/1900x1600) no-repeat center center/cover",
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default InfoSidebar;
--- components/login/SignInArea.tsx
+++ components/login/SignInArea.tsx
@@ -26,27 +26,33 @@ const SignInArea = () => {
   };
 
   return (
-    <div className="flex h-full w-1/2 flex-col items-center justify-center gap-4">
-      <Link href="/" className="absolute top-5 left-5 text-2xl font-semibold">
-        writedown
-      </Link>
-      <p className="mb-5 text-center font-medium">
-        Login with your familiar services:
-      </p>
-      <Button
-        extraClasses="flex gap-2 border-2 hover:!border-slate- !text-slate-900 !px-10"
-        data-testid="google-login"
-        onClick={() => login("google")}
-      >
-        <FcGoogle className="h-6 w-6" /> Sign in with Google
-      </Button>
-      <Button
-        extraClasses="flex gap-2 border-2 hover:!border-slate-600 !bg-slate-900 !text-slate-50 !px-10 hover:!bg-slate-600 hover:!text-slate-50"
-        data-testid="github-login"
-        onClick={() => login("github")}
-      >
-        <AiFillGithub className="h-6 w-6" /> Sign in with GitHub
-      </Button>
+    <div className="flex h-full flex-col items-center justify-end gap-4 bg-slate-200 sm:w-1/2 sm:justify-center sm:bg-gray-50 ">
+      <div className="top-10 left-5 px-4 pb-4 text-2xl font-semibold sm:absolute">
+        <Link href="/">writedown</Link>
+        <p className="pr-10 pt-4 text-sm font-medium sm:hidden">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa animi
+          alias id provident fugit, laudantium cumque.
+        </p>
+      </div>
+      <div className="bottom-0 flex w-full flex-col items-center gap-4 rounded-t-3xl bg-gray-50 px-10 py-16 pb-20">
+        <p className="mb-5 text-center text-sm font-medium">
+          Login with your familiar services:
+        </p>
+        <Button
+          extraClasses="flex gap-2 border-2 hover:!border-slate- !text-slate-900 !px-10"
+          data-testid="google-login"
+          onClick={() => login("google")}
+        >
+          <FcGoogle className="h-6 w-6" /> Sign in with Google
+        </Button>
+        <Button
+          extraClasses="flex gap-2 border-2 hover:!border-slate-600 !bg-slate-900 !text-slate-50 !px-10 hover:!bg-slate-600 hover:!text-slate-50"
+          data-testid="github-login"
+          onClick={() => login("github")}
+        >
+          <AiFillGithub className="h-6 w-6" /> Sign in with GitHub
+        </Button>
+      </div>
     </div>
   );
 };
