62e19d626be165943c4b4d003b28b1340f5aabc6
ImRayy <souradeep20roy01@gmail.com>
2023-04-17 10:40:40 +0530

    Updated login screen

--- components/login/InfoSidebar.tsx
+++ components/login/InfoSidebar.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-
 const InfoSidebar = () => {
-  return <div className="flex h-full w-1/2 flex-col gap-2 bg-slate-200"></div>;
+  return (
+    <div className="relative hidden h-full w-1/2 flex-col gap-2 overflow-hidden bg-slate-300 md:flex">
+      <h1 className="mt-52 ml-20 text-2xl font-semibold">
+        Simplicity starts here
+      </h1>
+      <p className="ml-20 w-1/2 text-slate-700">
+        A fast, easy and free way to write notes with offline support, cloud
+        sync and real-time markdown preview.
+      </p>
+      <img
+        src="/writedown.png"
+        alt="Writedown Screenshot"
+        className="absolute left-1/3 bottom-0 scale-150 rounded-xl shadow-lg shadow-slate-900/30"
+      />
+    </div>
+  );
 };
 
 export default InfoSidebar;
+//  <div className="hidden h-full w-1/2 flex-col gap-2 bg-slate-200 md:flex"></div>
--- components/login/SignInArea.tsx
+++ components/login/SignInArea.tsx
@@ -26,27 +26,38 @@ const SignInArea = () => {
   };
 
   return (
-    <div className="flex h-full w-1/2 flex-col items-center justify-center gap-4">
-      <Link href="/" className="absolute top-5 left-5 text-2xl font-semibold">
-        writedown
-      </Link>
-      <p className="mb-5 text-center font-medium">
-        Login with your familiar services:
-      </p>
-      <Button
-        extraClasses="flex gap-2 border-2 hover:!border-slate- !text-slate-900 !px-10"
-        data-testid="google-login"
-        onClick={() => login("google")}
-      >
-        <FcGoogle className="h-6 w-6" /> Sign in with Google
-      </Button>
-      <Button
-        extraClasses="flex gap-2 border-2 hover:!border-slate-600 !bg-slate-900 !text-slate-50 !px-10 hover:!bg-slate-600 hover:!text-slate-50"
-        data-testid="github-login"
-        onClick={() => login("github")}
-      >
-        <AiFillGithub className="h-6 w-6" /> Sign in with GitHub
-      </Button>
+    <div className="flex h-full flex-col items-center justify-end gap-4 bg-slate-300 sm:w-1/2 sm:justify-center sm:bg-gray-50 ">
+      <div className="top-0 left-5 px-4 pb-4 text-2xl font-semibold sm:absolute">
+        <Link href="/" className="absolute top-4">
+          writedown
+        </Link>
+        <p className="text-xl font-semibold sm:hidden">
+          Simplicity starts here
+        </p>
+        <p className="pr-10 pt-4 text-sm  font-normal sm:hidden">
+          A fast, easy and free way to write notes with offline support, cloud
+          sync and real-time markdown preview.
+        </p>
+      </div>
+      <div className="bottom-0 flex w-full flex-col items-center gap-4 rounded-t-3xl bg-gray-50 px-10 py-16 pb-20">
+        <p className="mb-5 text-center text-sm font-medium">
+          Login with your familiar services:
+        </p>
+        <Button
+          extraClasses="flex gap-2 border-2 hover:!border-slate- !text-slate-900 !px-10"
+          data-testid="google-login"
+          onClick={() => login("google")}
+        >
+          <FcGoogle className="h-6 w-6" /> Sign in with Google
+        </Button>
+        <Button
+          extraClasses="flex gap-2 border-2 hover:!border-slate-600 !bg-slate-900 !text-slate-50 !px-10 hover:!bg-slate-600 hover:!text-slate-50"
+          data-testid="github-login"
+          onClick={() => login("github")}
+        >
+          <AiFillGithub className="h-6 w-6" /> Sign in with GitHub
+        </Button>
+      </div>
     </div>
   );
 };
