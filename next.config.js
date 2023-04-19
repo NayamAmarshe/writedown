2351479ce2321fb437115f8d14d512008ca71e2a
TGS963 <ghoshsuvojit2012@gmail.com>
2023-04-19 18:10:20 +0530

    does not work in prod

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
   runtimeCaching,
   register: true,
-  disable: process.env.NODE_ENV === "development",
+  //disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
