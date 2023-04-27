b1736d258d446d112f287a3b337f978cb2878b47
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-04-27 21:22:17 +0530

    Added beta badge

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -1,3 +1,4 @@
+import BetaBadge from "../ui/BetaBadge";
 import Button from "../ui/Button";
 import Link from "next/link";
 import React from "react";
@@ -6,7 +7,9 @@ const Navbar = () => {
   return (
     <nav className="fixed z-10 flex w-full flex-row justify-between bg-slate-50/50 px-4 py-4 backdrop-blur-md md:px-10 lg:px-36">
       <div className="flex w-full flex-row items-center justify-start gap-2">
-        <h4 className="text-2xl font-semibold">writedown</h4>
+        <h4 className="flex items-center text-2xl font-semibold">
+          writedown <BetaBadge />
+        </h4>
       </div>
       <div className="flex w-full flex-row justify-end">
         <Link href="/login">
