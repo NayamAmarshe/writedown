1c804c4392aac6a4b769f89722199d9d706a7472
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-06-11 17:54:30 +0530

    Refactor code

--- components/dashboard/TextArea/PostButtons.tsx
+++ components/dashboard/TextArea/PostButtons.tsx
@@ -1,49 +1,39 @@
+import { postContentAtom, postTitleAtom } from "@/stores/editTextAreaAtom";
+import { selectedNoteIdAtom } from "@/stores/selectedChannelIdAtom";
 import { TNotesData } from "@/types/utils/firebaseOperations";
 import CloudArrowUp from "@/components/icons/CloudArrowUp";
 import { syncLoadingAtom } from "@/stores/syncLoadingAtom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import ArrowPath from "@/components/icons/ArrowPath";
+import useNotes from "@/components/hooks/useNotes";
+import { isSyncedAtom } from "@/stores/syncedAtom";
 import React, { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
+import { useAtom, useAtomValue } from "jotai";
 import Check from "@/components/icons/Check";
 import Trash from "@/components/icons/Trash";
 import Button from "@/components/ui/Button";
 import { toast } from "react-hot-toast";
-import { useAtomValue } from "jotai";
 import { auth } from "@/pages/_app";
 
 type PostButtonsProps = {
-  isSynced: boolean;
-  setIsSynced: React.Dispatch<React.SetStateAction<boolean>>;
-  selectedNoteId: string | null;
-  setSelectedNoteId: React.Dispatch<React.SetStateAction<string | null>>;
-  title: string;
-  input: string;
-  notes: TNotesData[] | undefined;
-  updateNote: (note: {
-    id: string;
-    title: string;
-    content: string;
-  }) => Promise<void>;
-  deleteNote: (id: string) => Promise<void>;
   shiftRight?: boolean;
 };
 
-const PostButtons = ({
-  isSynced,
-  setIsSynced,
-  selectedNoteId,
-  setSelectedNoteId,
-  title,
-  input,
-  notes,
-  updateNote,
-  deleteNote,
-  shiftRight,
-}: PostButtonsProps) => {
+const PostButtons = ({ shiftRight }: PostButtonsProps) => {
+  const [user] = useAuthState(auth);
+
   const [lastUpdated, setLastUpdated] = useState<string | null>(null);
 
+  const [selectedNoteId, setSelectedNoteId] = useAtom(selectedNoteIdAtom);
+  const postContent = useAtomValue(postContentAtom);
+  const postTitle = useAtomValue(postTitleAtom);
   const syncLoading = useAtomValue(syncLoadingAtom);
+  const [synced, setSynced] = useAtom(isSyncedAtom);
+
+  const { notes, updateNote, deleteNote } = useNotes({
+    userId: user?.uid,
+  });
 
   useEffect(() => {
     if (!notes || !selectedNoteId) return;
@@ -60,15 +50,15 @@ const PostButtons = ({
    * Saves the note if not already synced
    */
   const saveNoteHandler = () => {
-    if (syncLoading || isSynced) return;
+    if (syncLoading || synced) return;
     if (!selectedNoteId || !notes?.find((note) => note.id === selectedNoteId))
       return;
     updateNote({
       id: selectedNoteId,
-      title: title,
-      content: input,
+      title: postTitle,
+      content: postContent,
     });
-    setIsSynced(true);
+    setSynced(true);
   };
 
   /**
@@ -127,13 +117,13 @@ const PostButtons = ({
               <p>Saving</p>
             </span>
           )}
-          {!syncLoading && isSynced && (
+          {!syncLoading && synced && (
             <span className="flex items-center justify-center gap-1">
               <Check className="h-5 w-5" />
               <p>Saved</p>
             </span>
           )}
-          {!syncLoading && !isSynced && (
+          {!syncLoading && !synced && (
             <span className="flex items-center justify-center gap-1">
               <CloudArrowUp className="h-5 w-5" />
               <p>Save Note</p>
