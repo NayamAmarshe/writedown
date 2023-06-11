eec65792d82214d1cbf298ede2c714a3d33a9a4b
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-06-11 17:10:41 +0530

    Fixed syncing

--- components/dashboard/TextArea/index.tsx
+++ components/dashboard/TextArea/index.tsx
@@ -1,7 +1,7 @@
+import { postContentAtom, postTitleAtom } from "@/stores/editTextAreaAtom";
 import { ProsemirrorAdapterProvider } from "@prosemirror-adapter/react";
 import ChevronDoubleLeft from "@/components/icons/ChevronDoubleLeft";
 import { selectedNoteIdAtom } from "@/stores/selectedChannelIdAtom";
-import { inputAtom, titleAtom } from "@/stores/editTextAreaAtom";
 import MilkdownEditor from "@/components/ui/MilkdownEditor";
 import { useAuthState } from "react-firebase-hooks/auth";
 import IconButton from "@/components/ui/IconButton";
@@ -23,8 +23,8 @@ const TextArea = ({ shiftRight, setShiftRight }: TextAreaProps) => {
   const [user] = useAuthState(auth);
   const [selectedNoteId, setSelectedNoteId] = useAtom(selectedNoteIdAtom);
   const [synced, setSynced] = useAtom(isSyncedAtom);
-  const [title, setTitle] = useAtom(titleAtom);
-  const [input, setInput] = useAtom(inputAtom);
+  const [postTitle, setPostTitle] = useAtom(postTitleAtom);
+  const [postContent, setPostContent] = useAtom(postContentAtom);
   const [noteUnchanged, setNoteUnchanged] = useState(false);
   const { notes, updateNote, deleteNote, createNote } = useNotes({
     userId: user?.uid,
@@ -45,21 +45,21 @@ const TextArea = ({ shiftRight, setShiftRight }: TextAreaProps) => {
     if (!notes) return;
     if (notes.length > 0 && !selectedNoteId) {
       setSelectedNoteId(notes[0].id);
-      setInput(notes[0].content);
-      setTitle(notes[0].title);
+      setPostContent(notes[0].content);
+      setPostTitle(notes[0].title);
       return;
     }
     if (!selectedNoteId) return;
     const selectedNote = notes.find((note) => note.id === selectedNoteId);
     if (!selectedNote) return;
-    setInput(selectedNote.content);
-    setTitle(selectedNote.title);
+    setPostContent(selectedNote.content);
+    setPostTitle(selectedNote.title);
   }, [notes, selectedNoteId]);
 
   useEffect(() => {
     if (!selectedNoteId || !user) return;
     const currentNote = notes?.find(
-      (note) => input === note.content && title === note.title
+      (note) => postContent === note.content && postTitle === note.title
     );
     const isNoteUnchanged = currentNote?.id === selectedNoteId;
     if (isNoteUnchanged) {
@@ -70,21 +70,21 @@ const TextArea = ({ shiftRight, setShiftRight }: TextAreaProps) => {
       setNoteUnchanged(false);
       setSynced(false);
     }
-  }, [notes, title, input, synced]);
+  }, [notes, postTitle, postContent, synced]);
 
   useEffect(() => {
     const debounceSave = setTimeout(() => {
       if (!selectedNoteId || !user || noteUnchanged) return;
       updateNote({
         id: selectedNoteId,
-        title,
-        content: input,
+        title: postTitle,
+        content: postContent,
       });
-    }, 500);
+    }, 2000);
     return () => {
       clearTimeout(debounceSave);
     };
-  }, [input, title]);
+  }, [postContent, postTitle]);
 
   return (
     <div
@@ -105,8 +105,8 @@ const TextArea = ({ shiftRight, setShiftRight }: TextAreaProps) => {
         setSelectedNoteId={setSelectedNoteId}
         isSynced={synced}
         setIsSynced={setSynced}
-        input={input}
-        title={title}
+        input={postContent}
+        title={postTitle}
         notes={notes}
         updateNote={updateNote}
         shiftRight={shiftRight}
@@ -128,10 +128,10 @@ const TextArea = ({ shiftRight, setShiftRight }: TextAreaProps) => {
             type="text"
             className="w-full appearance-none border-none p-0 text-5xl font-bold leading-relaxed focus:outline-none focus:ring-0"
             onChange={(e) => {
-              setTitle(e.target.value);
+              setPostTitle(e.target.value);
             }}
             placeholder="Untitled"
-            value={title}
+            value={postTitle}
           />
 
           {/* SEPARATOR */}
@@ -139,8 +139,8 @@ const TextArea = ({ shiftRight, setShiftRight }: TextAreaProps) => {
 
           <ProsemirrorAdapterProvider>
             <MilkdownEditor
-              input={input}
-              setInput={setInput}
+              input={postContent}
+              setInput={setPostContent}
               className="prose !max-h-none min-h-screen !max-w-none p-2 focus:outline-none"
               notes={notes}
             />
