1a0a2b2c3fa8e44f384a70b783680e4fab718e4d
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-06-16 08:38:31 +0530

    Update updatedAt check

--- stores/postDataAtom.ts
+++ stores/postDataAtom.ts
@@ -2,3 +2,4 @@ import { atom } from "jotai";
 
 export const postTitleAtom = atom<string>("");
 export const postContentAtom = atom<string>("");
+export const postLastUpdatedAtom = atom<number | null>(null);
