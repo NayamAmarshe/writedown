641c31ba82e4f814ba361a5fd1bd5abbef072ff5
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-07-10 10:46:25 +0530

    Update colors

--- components/home/FeatureCard.tsx
+++ components/home/FeatureCard.tsx
@@ -8,7 +8,7 @@ type FeatureCardProps = {
 
 const FeatureCard = ({ icon, title, description }: FeatureCardProps) => {
   return (
-    <div className="flex w-full cursor-default flex-col gap-4 rounded-xl bg-slate-200 p-5 text-sm transition-all duration-300 hover:scale-105 hover:bg-sky-100 dark:bg-slate-700 sm:hover:scale-110">
+    <div className="flex w-full cursor-default flex-col gap-4 rounded-xl bg-slate-200 p-5 text-sm transition-all duration-300 hover:scale-105 hover:bg-slate-100 dark:bg-slate-700 sm:hover:scale-110">
       <h6 className="tex-base flex flex-col items-center gap-1 font-medium text-slate-900 dark:text-slate-100 sm:text-lg">
         {icon} {title}
       </h6>
