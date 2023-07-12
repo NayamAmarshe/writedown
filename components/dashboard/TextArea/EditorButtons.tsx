97f9493da9e25eda6d4112591c11a5f685e2062d
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-07-13 00:50:11 +0530

    Add new plugin

--- components/dashboard/TextArea/EditorButtons.tsx
+++ components/dashboard/TextArea/EditorButtons.tsx
@@ -128,7 +128,7 @@ const EditorButtons = ({ shiftRight, editor }: EditorButtonsProps) => {
         <button
           className="rounded-xl p-2 hover:bg-slate-200 dark:hover:bg-slate-700"
           onClick={() =>
-            editor.chain().focus().toggleCodeBlock().unsetCode().run()
+            editor.chain().focus().toggleDetails().unsetCode().run()
           }
         >
           <LuCode className="dark:text-slate-200" />
