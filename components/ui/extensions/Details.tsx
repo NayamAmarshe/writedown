a7e11a7c0b93fbe41c4c16eb36c8c918c4915ebe
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-07-13 01:00:04 +0530

    Fix button

--- components/ui/extensions/Details.tsx
+++ components/ui/extensions/Details.tsx
@@ -54,7 +54,6 @@ export const Details = Node.create<DetailsOptions>({
       {
         class: "details",
         "data-type": "details",
-        open,
       },
       [
         "button",
@@ -65,105 +64,6 @@ export const Details = Node.create<DetailsOptions>({
       ["div", ["summary"], ["div", { class: "details-content" }, 0]],
     ];
   },
-  addCommands() {
-    return {
-      toggleDetails:
-        (attributes) =>
-        ({ commands }) => {
-          console.log("hello");
-          return commands.toggleNode(this.name, "paragraph", attributes);
-        },
-    };
-  },
-
-  // addKeyboardShortcuts() {
-  //   return {
-  //     "Mod-Alt-c": () => this.editor.commands.toggleDetails(),
-
-  //     // remove code block when at start of document or code block is empty
-  //     Backspace: () => {
-  //       const { empty, $anchor } = this.editor.state.selection;
-  //       const isAtStart = $anchor.pos === 1;
-
-  //       if (!empty || $anchor.parent.type.name !== this.name) {
-  //         return false;
-  //       }
-
-  //       if (isAtStart || !$anchor.parent.textContent.length) {
-  //         return this.editor.commands.clearNodes();
-  //       }
-
-  //       return false;
-  //     },
-
-  //     // exit node on triple enter
-  //     Enter: ({ editor }) => {
-  //       if (!this.options.exitOnTripleEnter) {
-  //         return false;
-  //       }
-
-  //       const { state } = editor;
-  //       const { selection } = state;
-  //       const { $from, empty } = selection;
-
-  //       if (!empty || $from.parent.type !== this.type) {
-  //         return false;
-  //       }
-
-  //       const isAtEnd = $from.parentOffset === $from.parent.nodeSize - 2;
-  //       const endsWithDoubleNewline = $from.parent.textContent.endsWith("\n\n");
-
-  //       if (!isAtEnd || !endsWithDoubleNewline) {
-  //         return false;
-  //       }
-
-  //       return editor
-  //         .chain()
-  //         .command(({ tr }) => {
-  //           tr.delete($from.pos - 2, $from.pos);
-
-  //           return true;
-  //         })
-  //         .exitCode()
-  //         .run();
-  //     },
-
-  //     // exit node on arrow down
-  //     ArrowDown: ({ editor }) => {
-  //       if (!this.options.exitOnArrowDown) {
-  //         return false;
-  //       }
-
-  //       const { state } = editor;
-  //       const { selection, doc } = state;
-  //       const { $from, empty } = selection;
-
-  //       if (!empty || $from.parent.type !== this.type) {
-  //         return false;
-  //       }
-
-  //       const isAtEnd = $from.parentOffset === $from.parent.nodeSize - 2;
-
-  //       if (!isAtEnd) {
-  //         return false;
-  //       }
-
-  //       const after = $from.after();
-
-  //       if (after === undefined) {
-  //         return false;
-  //       }
-
-  //       const nodeAfter = doc.nodeAt(after);
-
-  //       if (nodeAfter) {
-  //         return false;
-  //       }
-
-  //       return editor.commands.exitCode();
-  //     },
-  //   };
-  // },
 
   addInputRules() {
     return [
@@ -177,30 +77,12 @@ export const Details = Node.create<DetailsOptions>({
     ];
   },
 
-  onToggle: (props) => {
-    console.log("onToggle", props);
-  },
-
   addProseMirrorPlugins() {
     return [
-      // this plugin creates a code block for pasted content from VS Code
-      // we can also detect the copied code language
       new Plugin({
         key: new PluginKey("detailsButton"),
         props: {
-          handleClick: (view, pos) => {
-            console.log("handleClick", pos);
-            const { dispatch, state } = view;
-            const { $from } = state.selection;
-            const node = $from.node($from.depth);
-            console.log("🚀 => file: Details.tsx:195 => node:", node);
-            // update the node attributes
-            const transaction = view.state.tr.setNodeMarkup(pos, undefined, {
-              open: !node.attrs.open,
-            });
-            dispatch(transaction);
-            return true;
-          },
+          handleClick: (view, pos) => {},
         },
       }),
     ];
